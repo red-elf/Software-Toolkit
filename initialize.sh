5f554aeb3b9e555ff2636fa2d83203556b50ab7f
Milos Vasic <milos85vasic@gmail.com>
2022-08-09 18:55:59 +0300

    Initialize scripts.

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+HERE="$(pwd)"
+ABOUT="ABOUT.txt"
+DIR_VERSION="Version"
+DIR_RECIPES="Recipes"
+DIR_DEPENDENCIES="Dependencies"
+SCRIPT_VERSION="version.sh"
 DEPENDABLE="git@github.com:red-elf/Dependable.git"
 INSTALLABLE="git@github.com:red-elf/Installable.git"
 VERSIONABLE="git@github.com:red-elf/Versionable.git"
@@ -10,7 +16,14 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+if [ -z "$2" ]; then
+
+  echo "ERROR: Please provide the project name"
+  exit 1
+fi
+
 TARGET="$1"
+PROJECT_NAME="$2"
 
 if test -e "$TARGET"; then
 
@@ -32,5 +45,18 @@ git status &&
   git submodule add "$INSTALLABLE" ./Installable && \
   git submodule add "$DEPENDABLE" ./Dependable && \
   git submodule add "$VERSIONABLE" ./Versionable && \
+  echo "$PROJECT_NAME" > "$ABOUT" && \
+  mkdir "$DIR_VERSION" && cd "$DIR_VERSION" && \
+  echo "#!/bin/bash
+export VERSIONABLE_SNAPSHOT=true
+export VERSIONABLE_NAME=\"$PROJECT_NAME\"
+export VERSIONABLE_VERSION_PRIMARY=\"1\"
+export VERSIONABLE_VERSION_SECONDARY=\"0\"
+export VERSIONABLE_VERSION_PATCH=\"0\"" > "$SCRIPT_VERSION" && \
+  chmod +x "$SCRIPT_VERSION" && \
+  cd .. && \
+  mkdir "$DIR_RECIPES" && cp "$HERE/Templates/Recipes/*.sh" "./$DIR_RECIPES" && \
+  cd .. && \
+  mkdir "$DIR_DEPENDENCIES"
   echo "The Software Toolkit has been initialized into: '$TARGET'"
 
