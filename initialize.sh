1d34003ffe80eace3629c6f8235904755bb34b52
Milos Vasic <milos85vasic@gmail.com>
2022-08-09 18:13:16 +0300

    Initialize submodules.

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DEPENDABLE="git@github.com:red-elf/Dependable.git"
+INSTALLABLE="git@github.com:red-elf/Installable.git"
+VERSIONABLE="git@github.com:red-elf/Versionable.git"
+
 if [ -z "$1" ]; then
 
   echo "ERROR: Please provide the target"
@@ -17,3 +21,9 @@ else
   echo "ERROR: The '$TARGET' directory does not exist"
   exit 1
 fi
+
+cd "$TARGET" && \
+  git status && \
+  git submodule add "$INSTALLABLE" ./Installable && \
+  git submodule add "$DEPENDABLE" ./Dependable && \
+  git submodule add "$VERSIONABLE" ./Versionable
