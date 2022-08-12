6a60ef7f4b86f8939ab42bffdf09d91a05426ca1
Milos Vasic <milos85vasic@gmail.com>
2022-08-12 19:05:54 +0300

    The update script - Improvements.

--- Scripts/update_software_toolkit.sh
+++ Scripts/update_software_toolkit.sh
@@ -5,42 +5,33 @@ DIR_DEPENDABLE="Dependable"
 DIR_VERSIONABLE="Versionable"
 DIR_INSTALLABLE="Installable"
 
-if test -e "$DIR_DEPENDABLE"; then
+function UPDATE_MODULE {
 
-  if ! cd "$DIR_DEPENDABLE" && git submodule update --remote; then
+  if [ -z "$1" ]; then
 
-    echo "ERROR: Could not update the Software Toolkit: '$DIR_DEPENDABLE'"
+    echo "ERROR: The Software Toolkit module directory not provided"
     exit 1
   fi
-else
 
-  echo "WARNING: Software Toolkit module '$DIR_DEPENDABLE' not found at '$(pwd)'"
-fi
+  DIR="$1"
 
-cd "$HERE" && \
+  if test -e "$DIR"; then
 
-if test -e "$DIR_VERSIONABLE"; then
+    if cd "$DIR" && git submodule update --remote && cd "$HERE"; then
 
-  if ! cd "$DIR_VERSIONABLE" && git submodule update --remote && cd "$HERE"; then
+      cd "$HERE" &&
+        echo "The Software Toolkit module updated: '$DIR'"
+    else
 
-    echo "ERROR: Could not update the Software Toolkit: '$DIR_VERSIONABLE'"
-    exit 1
+      echo "ERROR: Could not update the Software Toolkit: '$DIR'"
+      exit 1
+    fi
+  else
+
+    echo "WARNING: Software Toolkit module '$DIR' not found at '$(pwd)'"
   fi
-else
+}
 
-  echo "WARNING: Software Toolkit module '$DIR_VERSIONABLE' not found at '$(pwd)'"
-fi
-
-cd "$HERE" && \
-
-if test -e "$DIR_INSTALLABLE"; then
-
-  if ! cd "$DIR_INSTALLABLE" && git submodule update --remote && cd "$HERE"; then
-
-    echo "ERROR: Could not update the Software Toolkit: '$DIR_INSTALLABLE'"
-    exit 1
-  fi
-else
-
-  echo "WARNING: Software Toolkit module '$DIR_INSTALLABLE' not found at '$(pwd)'"
-fi
+UPDATE_MODULE "$DIR_DEPENDABLE"
+UPDATE_MODULE "$DIR_VERSIONABLE"
+UPDATE_MODULE "$DIR_INSTALLABLE"
