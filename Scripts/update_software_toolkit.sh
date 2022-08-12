77b8781f78605e1af1441739bccd66256a53d66a
Milos Vasic <milos85vasic@gmail.com>
2022-08-12 18:39:25 +0300

    The update script - Implementation.

--- Scripts/update_software_toolkit.sh
+++ Scripts/update_software_toolkit.sh
@@ -6,27 +6,36 @@ DIR_INSTALLABLE="Installable"
 
 if test -e "$DIR_DEPENDABLE"; then
 
-  if ! cd "$DIR_DEPENDABLE" && git submodule update --remote; then
+  if ! cd "$DIR_DEPENDABLE" && git submodule update --remote && cd .. ; then
 
     echo "ERROR: Could not update the Software Toolkit: '$DIR_DEPENDABLE'"
     exit 1
   fi
+else
+
+  echo "WARNING: Software Toolkit module '$DIR_DEPENDABLE' not found at '$(pwd)'"
 fi
 
 if test -e "$DIR_VERSIONABLE"; then
 
-  if ! cd "$DIR_VERSIONABLE" && git submodule update --remote; then
+  if ! cd "$DIR_VERSIONABLE" && git submodule update --remote && cd ..; then
 
     echo "ERROR: Could not update the Software Toolkit: '$DIR_VERSIONABLE'"
     exit 1
   fi
+else
+
+  echo "WARNING: Software Toolkit module '$DIR_VERSIONABLE' not found at '$(pwd)'"
 fi
 
 if test -e "$DIR_INSTALLABLE"; then
 
-  if ! cd "$DIR_INSTALLABLE" && git submodule update --remote; then
+  if ! cd "$DIR_INSTALLABLE" && git submodule update --remote && cd ..; then
 
     echo "ERROR: Could not update the Software Toolkit: '$DIR_INSTALLABLE'"
     exit 1
   fi
+else
+
+  echo "WARNING: Software Toolkit module '$DIR_INSTALLABLE' not found at '$(pwd)'"
 fi
