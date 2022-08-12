8afb17af30af778db3abc0c0bd9bbe958fba0ad8
Milos Vasic <milos85vasic@gmail.com>
2022-08-12 18:35:32 +0300

    The update script - Implementation.

--- Scripts/update_software_toolkit.sh
+++ Scripts/update_software_toolkit.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
-echo "TODO"
-exit 1
+DIR_DEPENDABLE="Dependable"
+DIR_VERSIONABLE="Versionable"
+DIR_INSTALLABLE="Installable"
+
+if test -e "$DIR_DEPENDABLE"; then
+
+  if ! cd "$DIR_DEPENDABLE" && git submodule update --remote; then
+
+    echo "ERROR: Could not update the Software Toolkit: '$DIR_DEPENDABLE'"
+    exit 1
+  fi
+fi
+
+if test -e "$DIR_VERSIONABLE"; then
+
+  if ! cd "$DIR_VERSIONABLE" && git submodule update --remote; then
+
+    echo "ERROR: Could not update the Software Toolkit: '$DIR_VERSIONABLE'"
+    exit 1
+  fi
+fi
+
+if test -e "$DIR_INSTALLABLE"; then
+
+  if ! cd "$DIR_INSTALLABLE" && git submodule update --remote; then
+
+    echo "ERROR: Could not update the Software Toolkit: '$DIR_INSTALLABLE'"
+    exit 1
+  fi
+fi
