fe6f7119107b2905d936e4679ca4999d9d221b95
Milos Vasic <i@mvasic.ru>
2023-01-13 17:50:28 +0300

    DB Toolkit - First touches.

---
+++ Utils/Db/get_postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DB="database.db"
+
+if [ -n "$1" ]; then
+
+  DB="$1"
+fi
+
+echo "ERROR: Not implemented"
+exit 1
---
+++ Utils/Db/get_sqlite.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DB="database.db"
+
+if [ -n "$1" ]; then
+
+  DB="$1"
+fi
+
+if test -e "$DB"; then
+
+  echo "Database is available: '$DB'"
+
+else
+
+  if echo "Initializing database: '$DB'" && sqlite3 "$DB" "VACUUM;" && test -e "$DB"; then
+
+    echo "Database is initialized: '$DB'"
+
+  else
+
+    echo "ERROR: database is not initialized: '$DB'"
+    exit 1
+  fi
+fi
