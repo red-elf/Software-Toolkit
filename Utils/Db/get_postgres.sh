700bcb234d92e46edb3c10ed6eb409373b3f9072
Milos Vasic <i@mvasic.ru>
2023-01-17 18:41:10 +0300

    Initialize Postgres Docker container

--- Utils/Db/get_postgres.sh
+++ Utils/Db/get_postgres.sh
@@ -3,15 +3,52 @@
 HERE="$(dirname -- "${BASH_SOURCE[0]}")"
 SCRIPT_GET_DOCKER="$HERE/../Sys/Programs/get_docker.sh"
 
-DB="database.db"
+DB="database"
 
 if [ -n "$1" ]; then
 
   DB="$1"
 fi
 
+USER="postgres"
+
+if [ -n "$2" ]; then
+
+  USER="$2"
+fi
+
+PASSWORD="$USER"
+
+if [ -n "$3" ]; then
+
+  PASSWORD="$3"
+fi
+
+# TODO: Refactor the Docker code to be used with any image
+
+DOCKER_IMAGE="postgres"
+DOCKER_CONTAINER="postgres.$DB"
+
 if sh "$SCRIPT_GET_DOCKER" true; then
 
+  if [ "$( docker container inspect -f '{{.State.Status}}' $DOCKER_CONTAINER )" == "running" ]; then
+
+    echo "Postgres Docker container is running"
+
+  else
+
+    if docker run --name "$DOCKER_CONTAINER" -e POSTGRES_USER="$USER" -e POSTGRES_PASSWORD="$PASSWORD" -e POSTGRES_DB="$DB" -d "$DOCKER_IMAGE"; then
+
+      echo "Postgres Docker container started"
+
+    else
+
+      echo "ERROR: Postgres Docker container failed to start"
+      exit 1
+    fi
+  fi
+
+  # TODO:
   echo "ERROR: Not implemented"
   exit 1
 
