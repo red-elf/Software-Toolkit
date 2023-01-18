92ecc36d11744a92eed71dcff7f8af6bedf544af
Milos Vasic <i@mvasic.ru>
2023-01-18 16:47:41 +0300

    The PGDATA parameter support.

--- Utils/Db/get_postgres.sh
+++ Utils/Db/get_postgres.sh
@@ -24,6 +24,32 @@ if [ -n "$3" ]; then
   PASSWORD="$3"
 fi
 
+DIRECTORY_DATA="/var/lib/postgresql/data/pgdata"
+
+if [ -n "$4" ]; then
+
+  DIRECTORY_DATA="$4"
+
+else
+
+  if test -e "$DIRECTORY_DATA"; then
+
+    echo "Directory available $DIRECTORY_DATA"
+
+  else
+
+    if mkdir -p "$DIRECTORY_DATA" then;
+
+      echo "Directory created $DIRECTORY_DATA"
+
+    else
+
+      echo "ERROR: directory not created $DIRECTORY_DATA"
+      exit 1
+    fi
+  fi
+fi
+
 # TODO: Refactor the Docker code to be used with any image
 
 DOCKER_IMAGE="postgres"
@@ -37,7 +63,8 @@ if sh "$SCRIPT_GET_DOCKER" true; then
 
   else
 
-    if docker run --name "$DOCKER_CONTAINER" -e POSTGRES_USER="$USER" -e POSTGRES_PASSWORD="$PASSWORD" -e POSTGRES_DB="$DB" -d "$DOCKER_IMAGE"; then
+    if docker run --name "$DOCKER_CONTAINER" -e POSTGRES_USER="$USER" -e POSTGRES_PASSWORD="$PASSWORD" \
+      -e POSTGRES_DB="$DB" -e PGDATA="$DIRECTORY_DATA" -d "$DOCKER_IMAGE"; then
 
       echo "Postgres Docker container started"
 
