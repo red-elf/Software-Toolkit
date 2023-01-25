b4023ba73e13d4b4c3ca26891127ede531ead178
Milos Vasic <i@mvasic.ru>
2023-01-25 17:50:28 +0300

    Urils: Open project script.

---
+++ Utils/Project/open_project_vscode.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+
+  echo "ERROR: Project parameter not ptovided"
+  exit 1
+fi
+
+PROJECT="$1"
+PROGAM="code"
+HERE="$(dirname -- "${BASH_SOURCE[0]}")"
+SCRIPT_GET_PROGRAM="$HERE/../Sys/Programs/get_program.sh"
+
+if sh "$SCRIPT_GET_PROGRAM" "$PROGAM"; then
+
+  "$PROGAM" "$PROJECT"
+
+else
+  
+  echo "ERROR: VSCode is not availble to open the project $PROJECT"
+  exit 1
+fi
