9a57625e295385ed8fb2bd022f6049c847946fdb
Milos Vasic <i@mvasic.ru>
2023-03-15 16:06:17 +0100

    Run the update script.

--- update_software_toolkit_recursively.sh
+++ update_software_toolkit_recursively.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 WHERE="$pwd"
+SCRIPT_UPDATE="update_software_toolkit.sh"
 
 if [ -n $1 ]; then
 
@@ -14,5 +15,10 @@ PATHS=$(find . -maxdepth 100 -mindepth 1 -type d)
 
 for PATH in $PATHS
 do
-    echo ">>> $PATH"
+    
+    if test -e "$PATH/$SCRIPT_UPDATE"; then
+
+      echo "Updatining by: $PATH/$SCRIPT_UPDATE" &&
+        cd "$PATH" && sh "$SCRIPT_UPDATE" && cd "$WHERE"
+    fi
 done
