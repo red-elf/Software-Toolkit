8b7e2e4feef0d9ae3583c083d34a00abddf8a0c2
Milos Vasic <i@mvasic.ru>
2023-04-02 16:29:28 +0300

    Get SonarQube - First touches.

---
+++ Utils/SonarQube/get_sonar_qube.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+HERE="$(dirname -- "${BASH_SOURCE[0]}")"
+SCRIPT_GET_DOCKER="$HERE/../Sys/Programs/get_docker.sh"
+
+DOCKER_IMAGE="sonarqube"
+DOCKER_TAG="9.9.0-community"
+DOCKER_CONTAINER="sonarqube"
+
+echo "Docker image: $DOCKER_IMAGE"
+echo "Docker container: $DOCKER_CONTAINER"
+
+if sh "$SCRIPT_GET_DOCKER" true; then
+
+  CONTAINER_STATUS="$( docker container inspect -f '{{.State.Status}}' $DOCKER_CONTAINER )"
+
+  if [ "$CONTAINER_STATUS" == "running" ]; then
+
+    echo "SonarQube Docker container is running"
+
+  else
+
+    if [ "$CONTAINER_STATUS" == "exited" ]; then
+
+      if docker container start "$DOCKER_CONTAINER"; then
+
+        echo "SonarQube Docker container re-started"
+
+      else
+
+        echo "ERROR: SonarQube Docker container failed to re-start"
+        exit 1
+      fi
+
+    else
+
+      if docker run -t "$DOCKER_CONTAINER:$DOCKER_TAG" -d "$DOCKER_IMAGE"; then
+
+        echo "Postgres Docker container started with the database"
+
+      else
+
+        echo "ERROR: SonarQube Docker container failed to start"
+        exit 1
+      fi
+
+    fi
+  fi
+  
+else
+
+  echo "ERROR: No Docker installation available"
+  exit 1
+fi
