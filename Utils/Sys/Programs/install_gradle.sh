63e4f48f8f85b4aa20312cdbdde8d24d79e40a6c
Milos Vasic <i@mvasic.ru>
2023-07-21 16:38:31 +0300

    Gradle installation script - Last touches.

--- Utils/Sys/Programs/install_gradle.sh
+++ Utils/Sys/Programs/install_gradle.sh
@@ -5,7 +5,10 @@ HERE="$(dirname -- "$0")"
 GRADLE_VERSION="8.2.1"
 DIR_INSTALLATION="/opt/gradle"
 GRADLE_ZIP="gradle-$GRADLE_VERSION.zip"
-DIR_DOWNLOADS="$(eval echo ~$USER)/Downloads"
+DIR_HOME="$(eval echo ~$USER)"
+DIR_DOWNLOADS="$DIR_HOME/Downloads"
+FILE_ZSH_RC="$DIR_HOME/.zshrc"
+FILE_BASH_RC="$DIR_HOME/.bashrc"
 SCRIPT_CHECK_ALT_LINUX="$HERE/../Recipes/altlinux.sh"
 URL_GRADLE_INSTALL="https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-all.zip"
 
@@ -55,6 +58,44 @@ if sh "$SCRIPT_CHECK_ALT_LINUX"; then
   if cd "$DIR_DOWNLOADS" && test -e "$GRADLE_ZIP"  && \
     sudo unzip -d "$DIR_INSTALLATION" "$GRADLE_ZIP" && ls "$DIR_INSTALLATION" && \
     cd "$CURRENT"; then
+
+    FILE_RC=""
+    
+    if test -e "$FILE_ZSH_RC"; then
+
+      FILE_RC="$FILE_ZSH_RC"
+
+    else
+
+        if test -e "$FILE_BASH_RC"; then
+
+          FILE_RC="$FILE_BASH_RC"
+
+        else
+
+          echo "ERROR: No '$FILE_ZSH_RC' or '$FILE_BASH_RC' found on the system"
+          exit 1
+        fi
+    fi
+
+    EXPORT_DIRECTIVE="export PATH=\${PATH}:$DIR_INSTALLATION/gradle-$GRADLE_VERSION/bin"
+
+    if cat "$FILE_RC" | grep "$EXPORT_DIRECTIVE"; then
+
+      echo "Gradle is already added into the system path"
+
+    else
+
+      if echo "" >> "$FILE_RC" && echo "$EXPORT_DIRECTIVE" >> "$FILE_RC"; then
+
+        echo "Gradle is added into the system path"
+
+      else
+
+        echo "ERROR: Gradle was not added into the system path with success"
+        exit 1
+      fi
+    fi
 
     exit 0
   fi
