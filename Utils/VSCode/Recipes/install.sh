81d6fd0d50bdeb33959b67c07b2b684f7847478b
Milos Vasic <i@mvasic.ru>
2023-08-01 16:42:09 +0300

    Working on the installation reciepes,

---
+++ Utils/VSCode/Recipes/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TODO: Extract installation and add to path
+# TODO: Install extensions (pickup some mandatory and additional from user's optional recipes)
+
+echo "ERROR: VSCode install script is not yet implemented"
+exit 1
---
+++ Utils/VSCode/Recipes/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO: Download and cache (pickup VSCode and some mandatory and additional extensions from user's optional recipes)
+
+echo "WARNING: VSCode prepare script is not yet implemented"
