c3e9336018a0b18e8baf7f8b8756987db9eb1827
Milos Vasic <i@mvasic.ru>
2023-08-01 17:39:28 +0300

    Scoping the installation.

--- Utils/VSCode/Recipes/prepare.sh
+++ Utils/VSCode/Recipes/prepare.sh
@@ -2,4 +2,8 @@
 
 # TODO: Download and cache (pickup VSCode and some mandatory and additional extensions from user's optional recipes)
 
+CURRENT="$(pwd)"
+
+echo "CURRENT: $CURRENT"
+
 echo "WARNING: VSCode prepare script is not yet implemented"
