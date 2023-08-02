d74642052e63d700100e7a9d100191e630aa7518
Milos Vasic <i@mvasic.ru>
2023-08-02 16:50:34 +0300

    Gather all Git submodules - First touches.

---
+++ Utils/Git/get_submodules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOCATION="."
+
+if [ -n "$1" ]; then
+
+    LOCATION="$1"
+fi
+
+echo "We are going to gather Git submodules information from: $LOCATION"
+
+echo "ERROR: Not yet implemented"
+exit 1
