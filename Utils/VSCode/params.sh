8aada74778cf6b36ee29b11e0df754fce0efb8bc
Milos Vasic <i@mvasic.ru>
2023-08-02 16:17:19 +0300

    Shellcheck ignore.

--- Utils/VSCode/params.sh
+++ Utils/VSCode/params.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 DIR_HOME="$(readlink --canonicalize ~)"
+
+# shellcheck disable=SC2034
 DIR_INSTALLATION_HOME="$DIR_HOME/VSCode"
 
+# shellcheck disable=SC2034
 DOWNLOAD_URL="https://code.visualstudio.com/sha/download?build=stable&os=linux-x64"
