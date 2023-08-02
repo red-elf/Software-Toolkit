09699db28679e4fce485fceb29cccb665443a86a
Milos Vasic <i@mvasic.ru>
2023-08-02 17:04:38 +0300

    Find all .gitmodules.

--- Utils/Git/get_submodules.sh
+++ Utils/Git/get_submodules.sh
@@ -9,5 +9,17 @@ fi
 
 echo "We are going to gather Git submodules information from: $LOCATION"
 
+DO_FILE() {
+
+    echo ">>> $1"
+}
+
+# shellcheck disable=SC2044
+for FILE in $(find "$LOCATION" -type f -name '.gitmodules');
+do
+    
+    DO_FILE "$FILE"
+done;
+
 echo "ERROR: Not yet implemented"
 exit 1
