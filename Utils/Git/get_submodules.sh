d556f01f19b08e474a831cb819524fb143fa6f11
Milos Vasic <i@mvasic.ru>
2023-08-03 16:13:08 +0300

    Parsing.

--- Utils/Git/get_submodules.sh
+++ Utils/Git/get_submodules.sh
@@ -9,6 +9,50 @@ fi
 
 echo "We are going to gather Git submodules information from: $LOCATION"
 
+check_prefixes () {
+    value=$1
+    shift
+
+    for prefix do
+        case $value in
+            "$prefix"*) return 0
+        esac
+    done
+
+    return 1
+}
+
+check_contains () {
+    value=$1
+    shift
+
+    case $value in
+        *"$1"*) return 0
+    esac
+
+    return 1
+}
+
+DO_SUBMODULE() {
+
+    if [ -z "$1" ]; then
+
+        echo "ERROR: Submodule name is mandatory parameter for the function"
+        exit 1
+    fi
+
+    if [ -z "$2" ]; then
+
+        echo "ERROR: Submodule repo is mandatory parameter for the function"
+        exit 1
+    fi
+
+    SUBMODULE="$1"
+    REPO="$2"
+    
+    echo "Processing Git submodule '$SUBMODULE': $REPO"
+}
+
 DO_FILE() {
 
     if [ -z "$1" ]; then
@@ -20,6 +64,10 @@ DO_FILE() {
     FILE="$1"
     
     echo "Processing Git modules file: $FILE"
+
+    REPO=""
+    SUBMODULE=""    
+    SUBMODULE_OPENING="[submodule"
     
     CONTENT=$(cat "$FILE")
 
@@ -27,7 +75,14 @@ DO_FILE() {
 
     for ITEM in $CONTENT; do
 
-        echo ">> $ITEM"
+        if check_prefixes "$ITEM" "$SUBMODULE_OPENING"; then
+
+            echo ">>>> $ITEM"
+
+        else
+
+            echo ">> $ITEM" 
+        fi
     done;
 }
 
