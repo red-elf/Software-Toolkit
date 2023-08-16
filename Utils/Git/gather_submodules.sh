99ca8d4fb000724f43f292828578b2d7daace112
Milos Vasic <i@mvasic.ru>
2023-08-16 18:05:12 +0300

    TODOs resolved.

--- Utils/Git/gather_submodules.sh
+++ Utils/Git/gather_submodules.sh
@@ -397,9 +397,7 @@ do
 done;
 
 # TODO: Extend push_all script so it:
-# - Introduce the global variable that will define the home _Submodules directory, scripts will use it if exported, othervise, 
-#   it will perform he default behavior regarding the paths
-# - Create the command shortcut for Upstreamable/push_all.sh -> push_all, with maybe some parameters
+#
 # - Obtains the info about the current module being pushed
 # - The it iterates recursively through the project and updates all modules to this commit
 # - After the update commit head changes and push them all to upstream with generic commit message and some additional info if needed.
