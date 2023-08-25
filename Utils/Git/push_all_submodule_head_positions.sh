875feb7531d024627a7c74143cc4988bb30c26e1
Milos Vasic <i@mvasic.ru>
2023-08-25 14:38:27 +0300

    Push sibmodules head postion changes recursively - First touches.

---
+++ Utils/Git/push_all_submodule_head_positions.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+DIR_HOME=$(eval echo ~"$USER")
+FILE_ZSH_RC="$DIR_HOME/.zshrc"
+FILE_BASH_RC="$DIR_HOME/.bashrc"
+
+
+FILE_RC=""
+    
+if test -e "$FILE_ZSH_RC"; then
+
+  FILE_RC="$FILE_ZSH_RC"
+
+else
+
+    if test -e "$FILE_BASH_RC"; then
+
+      FILE_RC="$FILE_BASH_RC"
+
+    else
+
+      echo "ERROR: No '$FILE_ZSH_RC' or '$FILE_BASH_RC' found on the system"
+      exit 1
+    fi
+fi
+
+# shellcheck disable=SC1090
+. "$FILE_RC"
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
+
+SCRIPT_STRINGS="$SUBMODULES_HOME/Software-Toolkit/Utils/strings.sh"
+
+if test -e "$SCRIPT_STRINGS"; then
+
+    # shellcheck disable=SC1090
+    . "$SCRIPT_STRINGS"
+
+else
+
+  echo "ERROR: Script not found '$SCRIPT_STRINGS'"
+  exit 1
+fi
+
+LOCATION="$(pwd)"
+
+if [ -n "$1" ]; then
+
+    LOCATION="$1"
+fi
+
+echo "We are going to push all submodule head positions recursively from: $LOCATION"
+
+
+
