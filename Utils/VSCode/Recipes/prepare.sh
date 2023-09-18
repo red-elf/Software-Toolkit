8e973efa2525d0d4d8855661f46e37be91e38209
Milos Vasic <i@mvasic.ru>
2023-09-18 18:07:58 +0300

    Download the installation.

--- Utils/VSCode/Recipes/prepare.sh
+++ Utils/VSCode/Recipes/prepare.sh
@@ -14,6 +14,7 @@ fi
 
 DIR_HOME="$(readlink --canonicalize ~)"
 DIR_DOWNLOADS="$DIR_HOME/Downloads"
+FILE_DOWNLOAD="$DIR_DOWNLOADS/VSCode_Installation.tar.gz"
 
 if ! test -e "$DIR_DOWNLOADS"; then
 
@@ -22,6 +23,25 @@ if ! test -e "$DIR_DOWNLOADS"; then
         echo "ERROR: Could not crete downloads directory '$DIR_DOWNLOADS'"
         exit 1
     fi
+fi
+
+if test -e "$FILE_DOWNLOAD"; then
+
+    if ! rm -f "$FILE_DOWNLOAD"; then
+
+        echo "ERROR: Could not remove downloaded file '$FILE_DOWNLOAD'"
+        exit 1
+    fi
+fi
+
+if wget "$DOWNLOAD_URL" -O "$FILE_DOWNLOAD"; then
+
+    echo "VSCode has been downloaded with success"
+
+else
+
+    echo "ERROR: VSCode has failed to downloaded"
+    exit 1
 fi
 
 
