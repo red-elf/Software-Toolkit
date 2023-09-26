fb8d785f9d3d2c281f861c5f498fe1a89c6c54ca
Milos Vasic <i@mvasic.ru>
2023-09-26 15:30:44 +0300

    Download the extensions archive.

---
+++ Utils/VSCode/Recipes/parameters.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+DOWNLOAD_URL_EXTENSIONS="https://disk.yandex.com/d/B7uqeMmOIouh5Q"
--- Utils/VSCode/Recipes/prepare.sh
+++ Utils/VSCode/Recipes/prepare.sh
@@ -12,10 +12,33 @@ if [ -z  "$DIR_INSTALLATION_HOME" ]; then
     exit 1
 fi
 
+HERE="$(dirname -- "$0")"
 DAY_CODE=$(date +%Y.%m.%d)
 DIR_HOME="$(readlink --canonicalize ~)"
 DIR_DOWNLOADS="$DIR_HOME/Downloads"
+FILE_PARAMETERS="$HERE/parameters.sh"
 FILE_DOWNLOAD="$DIR_DOWNLOADS/VSCode_Installation_$DAY_CODE.tar.gz"
+FILE_DOWNLOAD_EXTENSIONS="$DIR_DOWNLOADS/extensions_$DAY_CODE.tar.gz"
+
+if [ -z  "$DOWNLOAD_URL_EXTENSIONS" ]; then
+
+    if test -e "$FILE_PARAMETERS"; then
+
+        # shellcheck disable=SC1090
+        . "$FILE_PARAMETERS" >/dev/null 2>&1
+
+        if [ -z  "$DOWNLOAD_URL_EXTENSIONS" ]; then
+
+            echo "ERROR: The 'DOWNLOAD_URL_EXTENSIONS' variable is not defined"
+            exit 1
+        fi
+
+    else
+
+        echo "ERROR: File not found '$FILE_PARAMETERS'"
+        exit 1
+    fi
+fi
 
 if ! test -e "$DIR_DOWNLOADS"; then
 
@@ -26,19 +49,40 @@ if ! test -e "$DIR_DOWNLOADS"; then
     fi
 fi
 
-if test -e "$FILE_DOWNLOAD"; then
+DOWNLOAD_FILE() {
 
-    echo "File has been already downloaded: '$FILE_DOWNLOAD'"
-    
-else
+    if [ -z "$1" ]; then
 
-    if wget "$DOWNLOAD_URL" -O "$FILE_DOWNLOAD"; then
-
-        echo "VSCode has been downloaded with success"
-
-    else
-
-        echo "ERROR: VSCode has failed to downloaded"
+        echo "Download URL parameter is mandatory"
         exit 1
     fi
-fi
+
+    if [ -z "$2" ]; then
+
+        echo "Download destination parameter is mandatory"
+        exit 1
+    fi
+
+    DOWNLOAD_URL="$1"
+    FILE_DOWNLOAD="$2"
+
+    if test -e "$FILE_DOWNLOAD"; then
+
+        echo "File has been already downloaded: '$FILE_DOWNLOAD'"
+        
+    else
+
+        if wget "$DOWNLOAD_URL" -O "$FILE_DOWNLOAD"; then
+
+            echo "File '$FILE_DOWNLOAD' has been downloaded with success"
+
+        else
+
+            echo "ERROR: '$FILE_DOWNLOAD' has failed to downloaded"
+            exit 1
+        fi
+    fi
+}
+
+DOWNLOAD_FILE "$DOWNLOAD_URL" "$FILE_DOWNLOAD"
+DOWNLOAD_FILE "$DOWNLOAD_URL_EXTENSIONS" "$FILE_DOWNLOAD_EXTENSIONS"
