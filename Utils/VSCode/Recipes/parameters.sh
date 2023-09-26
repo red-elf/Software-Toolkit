77cd2fd89ae76b2fab1cd625f06dc2518fa702e2
Milos Vasic <i@mvasic.ru>
2023-09-26 16:58:20 +0300

    TODO added.

--- Utils/VSCode/Recipes/parameters.sh
+++ Utils/VSCode/Recipes/parameters.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: Provide a valid link:
+#
 # shellcheck disable=SC2034
-DOWNLOAD_URL_EXTENSIONS="https://disk.yandex.com/d/B7uqeMmOIouh5Q"
+# DOWNLOAD_URL_EXTENSIONS="https://wdho.ru/b5U9"
 
