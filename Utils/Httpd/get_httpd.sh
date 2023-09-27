2f85b99037e94883510512511229ce1ee5a8622e
Milos Vasic <i@mvasic.ru>
2023-09-27 17:35:20 +0300

    Get Docker check added.

--- Utils/Httpd/get_httpd.sh
+++ Utils/Httpd/get_httpd.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -n "$1" ]; then
   
   NAME="$1"
@@ -28,12 +30,28 @@ else
   exit 1
 fi
 
-if docker run -dit --name "$NAME" -p "$HTTPD_PORT":80 -v "$PWD":"$DIR_WEB_ROOT" httpd:2.4; then
+if [ -z "$SUBMODULES_HOME" ]; then
 
-    echo "Apache HTTPD has been started"
+  echo "ERROR: SUBMODULES_HOME not available"
+  exit 1
+fi
+
+SCRIPT_GET_DOCKER="$SUBMODULES_HOME/Utils/Sys/Programs/get_docker.sh"
+
+if sh "$SCRIPT_GET_DOCKER" true; then
+
+    if docker run -dit --name "$NAME" -p "$HTTPD_PORT":80 -v "$PWD":"$DIR_WEB_ROOT" httpd:2.4; then
+
+        echo "Apache HTTPD has been started"
+
+    else
+
+        echo "ERROR: Apache HTTPD was not started"
+        exit 1
+    fi
 
 else
 
-    echo "ERROR: Apache HTTPD was not started"
+    echo "ERROR: No mandatory installations (docker) available on the system"
     exit 1
 fi
