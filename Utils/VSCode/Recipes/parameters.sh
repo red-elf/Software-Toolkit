f049c0b6ca0a1aa2f66e3c50636a31f4af71b073
Milos Vasic <i@mvasic.ru>
2023-09-27 18:06:38 +0300

    Download URL updated.

--- Utils/VSCode/Recipes/parameters.sh
+++ Utils/VSCode/Recipes/parameters.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: Provide a valid link:
-#
 # shellcheck disable=SC2034
-# DOWNLOAD_URL_EXTENSIONS="https://wdho.ru/b5U9"
+DOWNLOAD_URL_EXTENSIONS="http://127.0.0.1:8081/extensions.1.0.0.tar.gz"
 
