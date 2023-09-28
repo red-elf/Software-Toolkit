2af663433a3b5c39af45064cc0f1b96d4ce78ea0
Milos Vasic <i@mvasic.ru>
2023-09-28 14:16:33 +0300

    URL updated.

--- Utils/VSCode/Recipes/parameters.sh
+++ Utils/VSCode/Recipes/parameters.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # shellcheck disable=SC2034
-DOWNLOAD_URL_EXTENSIONS="http://127.0.0.1:8081/extensions.1.0.0.tar.gz"
+DOWNLOAD_URL_EXTENSIONS="http://general-server.local:8081/extensions.1.0.0.tar.gz"
 
