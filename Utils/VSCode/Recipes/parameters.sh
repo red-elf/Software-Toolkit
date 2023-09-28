2ee3e16ce4e0da60e06dbdc2fea32ff024cd1489
Milos Vasic <i@mvasic.ru>
2023-09-28 14:26:22 +0300

    Targeting general local server.

--- Utils/VSCode/Recipes/parameters.sh
+++ Utils/VSCode/Recipes/parameters.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# shellcheck disable=SC2034
-DOWNLOAD_URL_EXTENSIONS="http://general-server.local:8081/extensions.1.0.0.tar.gz"
+if [ -z "$GENERAL_SERVER" ]; then
 
+    echo "ERROR: 'GENERAL_SERVER' variable is not set"
+    exit 1
+fi
+
+# shellcheck disable=SC2034
+DOWNLOAD_URL_EXTENSIONS="http://$GENERAL_SERVER:8081/extensions.1.0.0.tar.gz"
