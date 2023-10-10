b25ea2824aebee88ec1395a9205978be9bc4b1d2
Milos Vasic <i@mvasic.ru>
2023-10-10 15:11:21 +0300

    Work on the script continued.

--- Utils/SonarQube/configure_sonar_lint.sh
+++ Utils/SonarQube/configure_sonar_lint.sh
@@ -7,23 +7,72 @@ if [ -z "$SUBMODULES_HOME" ]; then
 fi
 
 RECIPE_SETTINGS_JSON="$SUBMODULES_HOME/Software-Toolkit/Utils/SonarQube/settings.json.sh"
+SCRIPT_GET_PROGRAM="$SUBMODULES_HOME/Software-Toolkit/Utils/Sys/Programs/get_program.sh"
+SCRIPT_EXTEND_JSON="$SUBMODULES_HOME/Software-Toolkit/Utils/VSCode/extend_settings_json.sh"
 
-if [ -n "$1" ]; then
+if ! test -e "$SCRIPT_GET_PROGRAM"; then
 
-  RECIPE_SETTINGS_JSON="$1"
-fi
-
-if test -e "$RECIPE_SETTINGS_JSON"; then
-
-  echo "Settings.json recipe: '$RECIPE_SETTINGS_JSON'"
-
-else
-
-  echo "ERROR: Recipe not found '$RECIPE_SETTINGS_JSON'"
+  echo "ERROR: Script not found '$SCRIPT_GET_PROGRAM'"
   exit 1
 fi
 
-# TODO: Merge with system level settings.json
+if ! test -e "$SCRIPT_EXTEND_JSON"; then
 
-echo "ERRO: Not yet implemented"
-exit 1
+  echo "ERROR: Script not found '$SCRIPT_EXTEND_JSON'"
+  exit 1
+fi
+
+if sh "$SCRIPT_GET_PROGRAM" code; then
+
+  CODE_PATH=$(which code)
+
+  if ! test -e "$CODE_PATH"; then
+
+    echo "ERROR: VSCode Path not found '$CODE_PATH'"
+    exit 1
+  fi
+
+  SETTINGS_JSON="$CODE_PATH/data/user-data/User/settings.json"
+
+  if test -e "$SETTINGS_JSON"; then
+
+    echo "Settings JSON: '$SETTINGS_JSON'"
+
+  else
+
+    if touch "$SETTINGS_JSON"; then
+
+      echo "Settings JSON created: '$SETTINGS_JSON'"
+
+    else
+
+      echo "ERROR: Could not create '$SETTINGS_JSON'"
+      exit 1
+    fi
+  fi
+
+  if [ -n "$1" ]; then
+
+    RECIPE_SETTINGS_JSON="$1"
+  fi
+
+  if test -e "$RECIPE_SETTINGS_JSON"; then
+
+    echo "Settings.json recipe: '$RECIPE_SETTINGS_JSON'"
+
+  else
+
+    echo "ERROR: Recipe not found '$RECIPE_SETTINGS_JSON'"
+    exit 1
+  fi
+
+  if sh "$SCRIPT_EXTEND_JSON" "$RECIPE_SETTINGS_JSON"; then
+
+    echo "Settings JSON has been extended with success"
+
+  else
+
+    echo "ERROR: Settings JSON failed to extend"
+    exit 1
+  fi
+fi
