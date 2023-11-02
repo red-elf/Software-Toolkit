6237d931f54a638fdd23c63e3f3012d57be7b84a
Milos Vasic <i@mvasic.ru>
2023-11-02 20:49:32 +0300

    Recipes parameter.

--- Utils/Git/install_private_submodules.sh
+++ Utils/Git/install_private_submodules.sh
@@ -2,12 +2,19 @@
 
 if [ -z "$1" ]; then
 
-    echo "ERROR: Private modules directory parameter is mandatory"
+    echo "ERROR: Private modules home directory parameter is mandatory"
     exit 1
 fi
 
-DIR_MODULES="$1"
+if [ -z "$2" ]; then
 
-echo "Checking and installing private modules from: '$DIR_MODULES'"
+    echo "ERROR: Private modules recipes directory parameter is mandatory"
+    exit 1
+fi
+
+DIR_MODULES_HOME="$1"
+DIR_RECIPES="$2"
+
+echo "Checking and installing private modules from '$DIR_RECIPES' into '$DIR_MODULES_HOME'"
 
 # TODO: Implement
