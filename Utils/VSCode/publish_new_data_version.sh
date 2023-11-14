1f211d9f5dd332922ebd86db5519989b4ee0228c
Milos Vasic <i@mvasic.ru>
2023-11-14 18:14:57 +0300

    Publish new data version - Frist tocuhes.

---
+++ Utils/VSCode/publish_new_data_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
+
+# TODO: Implement
+
+echo "ERROR: Publish new data version is not implemented yet"
+exit 1
