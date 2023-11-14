1979d26785062446b4a3a1d8939d8d19051f523b
Milos Vasic <i@mvasic.ru>
2023-11-15 00:06:56 +0300

    Commit script - First touches.

---
+++ Utils/Git/commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+    echo "ERROR: The SUBMODULES_HOME is not defined"
+    exit 1
+fi
+
+# TODO: Implement
+
+echo "ERROR: Commit is not yet implemented"
+exit 1
