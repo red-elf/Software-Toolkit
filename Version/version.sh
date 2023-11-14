e0777b2c95e5f3b4364d5066741f5dc79206e9b7
Milos Vasic <i@mvasic.ru>
2023-11-14 15:22:11 +0300

    Version set.

---
+++ Version/version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export VERSIONABLE_SNAPSHOT=true
+export VERSIONABLE_NAME="Software Toolkit"
+export VERSIONABLE_VERSION_PRIMARY="1"
+export VERSIONABLE_VERSION_SECONDARY="0"
+export VERSIONABLE_VERSION_PATCH="0"
+export VERSIONABLE_HOMEPAGE="https://github.com/red-elf/Software-Toolkit"
+export VERSIONABLE_DESCRIPTION="The set of tools and small utilities required to develop and build your projects."
