7df7c0cc7a7476b5d939cc4ad4f2473996fa62d5
Milos Vasic <i@mvasic.ru>
2023-11-19 18:38:48 +0300

    "Fonts destination directory."

--- Utils/Sys/Fonts/install_fonts.sh
+++ Utils/Sys/Fonts/install_fonts.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 DIR_SOURCE="Assets/Fonts"
+DIR_HOME=$(eval echo ~"$USER")
+DIR_DESTINATION="$DIR_HOME/.local/share/fonts"
 
 if [ -n "$1" ]; then
 
     DIR_SOURCE="$1"
+fi
+
+if [ -n "$2" ]; then
+
+    DIR_DESTINATION="$2"
 fi
 
 if ! test -e "$DIR_SOURCE"; then
@@ -12,3 +19,12 @@ if ! test -e "$DIR_SOURCE"; then
     echo "ERROR: The fonts source directory not found '$DIR_SOURCE'"
     exit 1
 fi
+
+if ! test -e "$DIR_DESTINATION"; then
+
+    echo "ERROR: The fonts destination directory not found '$DIR_DESTINATION'"
+    exit 1
+fi
+
+echo "Installing fonts into: '$DIR_DESTINATION'"
+
