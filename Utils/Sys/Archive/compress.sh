f06daf840fa29f680cfc62af6a8fc3ebd65db02b
Milos Vasic <i@mvasic.ru>
2023-11-25 16:16:04 +0300

    "Do compress - Testing."

--- Utils/Sys/Archive/compress.sh
+++ Utils/Sys/Archive/compress.sh
@@ -2,6 +2,8 @@
 
 COMPRESS() {
 
+    HERE="$(pwd)"
+
     if [ -z "$1" ]; then
 
         echo "ERROR: The 'WHAT' parameter is mandatory"
@@ -17,9 +19,15 @@ COMPRESS() {
     WHAT="$1"
     WHERE="$2"
 
+    # DIR_WHAT=$(dirname -- "$WHAT")
+
     echo "Compressing '$WHAT' into '$WHERE'"
 
-    if ! tar -cjf "$WHERE" -C "$WHAT" .; then
+    if tar -cjf "$WHERE" -C "$WHAT" .; then
+
+        cd "$HERE" && echo "Compressing '$WHAT' into '$WHERE' has completed"
+
+    else
 
         echo "ERROR: compressing '$WHAT' into '$WHERE' has failed"
         exit 1
